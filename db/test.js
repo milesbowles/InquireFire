fe1c3369c54ce90d13985b6327be19e7d4268b2c
Ernesto Hernandez <h_ernesto@ymail.com>
2018-01-01 22:08:18 -0800

    finished main questions

--- db/test.js
+++ db/test.js
@@ -1,8 +1,13 @@
 var db = require("../models");
-// db.Inquiry.create({
-//     q: "Inertia is the..",
+/** To create a new element on the questions table */
+// db.Inquiry.update({
+//     q: "Assuming no air resistance, what is the final speed of an object dropped from 3 meters?",
 //     subcategory: "physics",
-//     round: 1
+//     round: 2
+// }, {
+//     where: {
+//         id: 22
+//     }
 // }).then(function(dbInquiry){
 //     dbInquiry
 // });
@@ -10,26 +15,36 @@ var db = require("../models");
 //     console.log("redy");
 // });
 
-// db.Choice.create({
-//     c1: "weight of object",
-//     c2: "momentum of object",
-//     c3: "resistance to movement",
-//     c4: "distance from earth to moon",
-//     ans: "resistance to movement",
-//     InquiryId: 21
+
+/** To create a new element on the multiple choices table */
+// db.Choice.update({
+//     c1: "59 m/s",
+//     c2: "7.7 m/s",
+//     c3: "98 m/s",
+//     c4: "none of the above",
+//     ans: "7.7 m/s",
+//     InquiryId: 22
+// }, {
+//     where: {
+//         id: 22
+//     }
 // }).then(function(dbChoice){
 //     console.log(dbChoice);
 // });
 
 
-
-// db.Inquiry.findAll({}).then(function(dbInquiry){
-//    console.log(dbInquiry);
-// }); 
-
-db.Choice.findAll({}).then(function(dbChoice){
-    console.log(dbChoice);
-});
+//** Code to find the questions from the questions table */
+db.Inquiry.findAll({}).then(function(dbInquiry){
+   for (var i = 0; i < dbInquiry.length; i++){
+       console.log(dbInquiry[i].id + " " + dbInquiry[i].q + " " + dbInquiry[i].round);
+   }
+}); 
+/** Code to find the choices embedded in the multiple choices table */
+// db.Choice.findAll({}).then(function(dbChoice){
+//     for (var i = 0; i < dbChoice.length; i++){
+//         console.log(dbChoice[i].id + ": " + dbChoice[i].c1 + ", " + dbChoice[i].c2 + ", " + dbChoice[i].c3 + ", " + dbChoice[i].c4 + ", " + dbChoice[i].ans);
+//     }
+// });
 
 
 // db.sequelize.sync().then(function(){
